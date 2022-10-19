2626fde5627613253f95d63c99fd46747675d1fd
grauconejo13 <vanessaf.victorino@yahoo.com>
2022-10-19 01:18:41 -0500

    mongoDB connection 0.0.0.0/0 to Heroku success and fix CSS for responsive website

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-export const Secret = "MySecret";
+export const Secret = "MyCrazySecret";
 export const MongoURI = "mongodb+srv://vievie:I1vMbVHHkFyBLPPW@grauconejo13.hpjmuez.mongodb.net/?retryWrites=true&w=majority"
